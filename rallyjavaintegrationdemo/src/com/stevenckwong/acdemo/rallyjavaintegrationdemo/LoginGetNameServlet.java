f58d4f39e87bb28505c8b1142d29d7be3ef5a82e
steven <me@stevenckwong.com>
2017-10-18 11:35:32 +1100

    US248: Demo Query on User using Java Integration
    Task-Url: https://rally1.rallydev.com/#/162105565492u/detail/userstory/165857955392

---
+++ rallyjavaintegrationdemo/src/com/stevenckwong/acdemo/rallyjavaintegrationdemo/LoginGetNameServlet.java
@@ -0,0 +1,75 @@
+package com.stevenckwong.acdemo.rallyjavaintegrationdemo;
+
+import java.io.IOException;
+import java.net.URI;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.rallydev.rest.*;
+
+/**
+ * Servlet implementation class LoginGetNameServlet
+ */
+@WebServlet(description = "This servlet logs in using a username and password and then returns the Display name of the user", urlPatterns = { "/LoginGetNameServlet" })
+public class LoginGetNameServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginGetNameServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		URI uri = URI.create("https://rally1.rallydev.com");
+		
+		RallyRestApi rally = new RallyRestApi(uri, username, password);
+		String QueryString = "(UserName%20%3D%20"+username+")&start=1&pagesize=20";
+		String queryURL = "/user?query=" + QueryString + "&order=";
+
+		// Sample request from documentation
+		// https://rally1.rallydev.com/slm/webservice/v2.0/user?workspace=https://rally1.rallydev.com/slm/webservice/v2.0/workspace/146174538760&query=(UserName%20%3D%20stevenck.wong%40acme.com)&start=1&pagesize=20
+	
+		// String queryURL = "/user?workspace=https://rally1.rallydev.com/slm/webservice/v2.0/workspace/146174538760&query=";
+		
+		
+		String result = rally.getClient().doGet(queryURL);
+
+		int startIndex = result.indexOf("\"_refObjectName\": \"");
+		startIndex+=19;
+		
+		int endIndex = result.indexOf("\"", startIndex);
+		
+		String displayName = result.substring(startIndex, endIndex);
+		
+		rally.close();
+		
+		response.getWriter().append("<h1>Display Name is " + displayName + "</h1>");
+		
+		
+		doGet(request, response);
+	}
+
+}
