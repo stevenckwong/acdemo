54479807df73400c9ee26cfe594a93e134cb4bf6
steven <steven@SW-Ubuntu16>
2017-11-01 14:15:10 +1100

    US249: Test Jenkins Build with Simple Java Application
    Task-Url: https://rally1.rallydev.com/#/162105565492u/detail/userstory/167577204044

--- rallyjavaintegrationdemo/src/com/stevenckwong/acdemo/rallyjavaintegrationdemo/JenkinsBuildJavaClass.java
+++ rallyjavaintegrationdemo/src/com/stevenckwong/acdemo/rallyjavaintegrationdemo/JenkinsBuildJavaClass.java
@@ -8,6 +8,7 @@ public class JenkinsBuildJavaClass {
 	
 	static public void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("This java class is for testing the Jenkins build integration");
 	}
 	
 
